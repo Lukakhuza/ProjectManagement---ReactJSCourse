3c7dff8726513535722de5ef271df762d041082a
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2024-05-21 12:42:59 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsList from "./components/ProjectsSidebar";
+import { useState } from "react";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  // function handleStartAddProject() {
+  //   setProjectsState((prevState)=> {...projectsState, selectedProjectId: 1}
+  // }}
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddproject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+  console.log(projectsState.selectedProjectId);
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddproject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    {
+      content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+    }
+  }
+  console.log(projectsState.selectedProjectId);
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsList
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {/* <NewProject /> */}
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject} /> */}
+      {content}
+    </main>
+  );
+}
+
+export default App;
